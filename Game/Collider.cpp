78693534ec8cb804a0dce772d76894359b7a56ee
Plut0og <gogleboywerser@gmail.com>
2019-11-08 11:34:10 -0500

    I'm fucking clueless about a good primitive system

--- Game/Collider.cpp
+++ Game/Collider.cpp
@@ -1,1 +1,37 @@
 #include "Collider.hpp"
+
+float min(float a, float b){
+    if(a > b){
+        return b;
+    } else {
+  	return a;
+    }
+}
+
+float max(float a, float b){
+    if(a > b){
+	return a;
+    } else {
+	return b;
+    } 
+}
+
+Collider::Collider(float _width, float _height, float _depth) : Component("collider") {
+    type = PRISM;
+    dimensions = glm::vec3(_width, _height, _depth);
+    offset = glm::vec3(0, 0, 0);
+}
+
+Collider::Collider(float _width, float _height, float _depth, float x, float y, float z) : Component("collider") {
+    type = PRISM;
+    
+}
+
+void start(){
+    //register to some kind've scene manager(stores collideables)
+}
+void update(){
+    //check for collisions and send messages
+    //update my bounding box
+
+}
--- Game/Collider.hpp
+++ Game/Collider.hpp
@@ -1,9 +1,39 @@
 #pragma once
 
 #include "Component.hpp"
+#include "Transform.hpp"
+
+//may break these into their own files later
+
+//bounding boxes will be used for preliminary collision detection
+class BoundingBox {
+public:
+    bool didCollide(BoundingBox* _box);
+    friend BoundingBox;
+private:
+    glm::vec3 position; 
+    glm::vec3 dimensions;
+}
 
 class Collider : public Component {
 public:
-    
+    Collider(float _radius);
+    Collider(float _width, float _height, float _depth);
+    Collider(float _radius, float x, float y, float z);
+    Collider(float _width, float _height, float _depth, float x, float y, float z);
+  
+    glm::vec3 getCenter(); 
+  
+    virtual void start();
+    virtual void update();  
 private:
+    enum Type {
+        PRISM,
+        SPHERE
+    };
+    
+    BoundingBox box;
+    glm::vec3 dimensions; 
+    glm::vec3 offset;
 }
+
